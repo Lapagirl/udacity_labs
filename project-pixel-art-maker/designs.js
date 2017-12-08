2cf743f0aa16f0be7ebcd95fb1f8dcb066d16c6a
Anastasia Savchikhina <anastasia@recolon.ru>
2017-12-08 23:03:00 +0300

    change hint color when error in the field

--- project-pixel-art-maker/designs.js
+++ project-pixel-art-maker/designs.js
@@ -1,11 +1,13 @@
 //show popup function
 function showPopup() {
     $('#popupCanvasProp').css('display', 'block');
+    $('.popup-overlay').css('display', 'block');
 }
 
 //hide popup function
 function hidePopup() {
     $('#popupCanvasProp').css('display', 'none');
+    $('.popup-overlay').css('display', 'none');
 }
 
 //show popup after page was loaded
@@ -40,29 +42,38 @@ function makeGrid() {
     }
 }
 
-
 //listener on submit button
-$('input[type="submit"]').click(function(event){
+$('#applyButton').click(function(event){
     var heightField = $('#input_height');
     var widthField = $('#input_width');
     var gridHeight = heightField.val();
     var gridWidth = widthField.val();
+    var widthHint = $('label[for="input_width"] .hint');
+    var heightHint = $('label[for="input_height"] .hint');
+    var isCorrect = true;
 
     event.preventDefault();
     heightField.css('border-color', 'rgba(43, 70, 117, 0.4)');
     widthField.css('border-color', 'rgba(43, 70, 117, 0.4)');
+    widthHint.css('color', 'gray');
+    heightHint.css('color', 'gray');
 
-    if (gridHeight > 0 && gridHeight <=100 && gridWidth > 0 && gridWidth <=100) {
+    if (gridHeight <= 0 || gridHeight > 100) {
+        $(heightField).css('border-color', 'red');
+
+        heightHint.css('color', 'red');
+        isCorrect = false;
+    }
+    if(gridWidth <= 0 || gridWidth > 100) {
+        $(widthField).css('border-color', 'red');
+        widthHint.css('color', 'red');
+        isCorrect = false;
+    }
+    if(isCorrect) {
         hidePopup();
         $('#canvasHeading').text("Design canvas");
         $('#pixel_canvas').children().remove();
         makeGrid();
-    }
-    if (gridHeight <= 0 || gridHeight > 100) {
-        $(heightField).css('border-color', 'red');
-    }
-    if(gridWidth <= 0 || gridWidth > 100) {
-        $(widthField).css('border-color', 'red');
     }
 }
 );
